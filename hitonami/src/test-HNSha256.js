8b0ddf146093d2370cc1f614e4a31bc1fbcbacd3
Luzi82 <luzi82@gmail.com>
2015-05-29 00:47:09 +0800

    improve null/empty case

--- hitonami/src/test-HNSha256.js
+++ hitonami/src/test-HNSha256.js
@@ -7,18 +7,10 @@ ut.addCase("pPHesjhW HNSha256",function(){
 	ut.eq("hIuvKdzl",s.toHex(),"98c8abc1c2d429660bd4d6fe12706fa0c3e4f70f68580ad1fc006f35888b9c38");
 });
 
-ut.addCase("pPHesjhW HNSha256 empty",function(){
-	var d = new hn.Data();
-	ut.eq("iKWswsLU",d.getSize(),0);
-	ut.t("zMdoexlv",d.isNull());
+ut.addCase("pPHesjhW HNSha256 null/empty",function(){
+	ut.eq("hIuvKdzl",hn.Sha256.getHash(new hn.Data()),null);
 
-	var s = hn.Sha256.getHash(d);
-	ut.eq("hIuvKdzl",s.toHex(),"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
-	
-	d = hn.Data.fromHex("");
-	ut.eq("iKWswsLU",d.getSize(),0);
-	ut.t("zMdoexlv",!d.isNull());
+	ut.eq("DTozGjbt",hn.Sha256.getHash(null),null);
 
-	s = hn.Sha256.getHash(d);
-	ut.eq("hIuvKdzl",s.toHex(),"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+	ut.eq("vNvonujO",hn.Sha256.getHash(hn.Data.fromHex("")),"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 });
